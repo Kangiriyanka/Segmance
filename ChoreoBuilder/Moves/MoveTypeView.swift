b037acc4b42de8357c573cc665545c56ec5111f4
Joe Farah <joseph.farah100@gmail.com>
2025-11-30 18:26:22 -0500

    Add error handling for MoveTypeView

--- ChoreoBuilder/Moves/MoveTypeView.swift
+++ ChoreoBuilder/Moves/MoveTypeView.swift
@@ -8,116 +8,99 @@
 import SwiftUI
 
 struct MoveTypeView: View {
-    
     var moveType: MoveType
     @Environment(\.modelContext) private var modelContext
     @Environment(\.dismiss) private var dismiss
+    
     @State private var typeName: String
     @State private var isPresentingConfirm: Bool = false
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError: Bool = false
+    @State private var characterLimit: Int = 25
+    
+    @FocusState private var isFocused: Bool
     
     var body: some View {
         ZStack {
-            
             NavigationStack {
-                
                 ScrollView {
-                    
                     VStack {
-                        
-                      
                         TextField("Type name", text: $typeName)
                             .bubbleStyle()
-                            
-                           
-                     
+                            .limitText($typeName, to: characterLimit)
+                            .focused($isFocused)
                     }
                     .padding()
-                      
-                    
-                    
-                     
-                        .toolbar {
-                            
-                            ToolbarItem {
-                                Button {
+                    .toolbar {
+                        ToolbarItem {
+                            Button {
+                                if validateTypeName() {
                                     moveType.name = typeName
                                     dismiss()
-                                } label: {
-                                    Image(systemName: "checkmark.circle")
                                 }
-                                
+                            } label: {
+                                Image(systemName: "checkmark.circle")
                             }
-                            
-                            ToolbarItem(placement: .navigationBarTrailing) {
-                                Menu {
-                                    Button("Delete", systemImage: "trash", role: .destructive) {
-                                        isPresentingConfirm = true
-                                    }
-                                } label: {
-                                    Image(systemName: "ellipsis.circle")
+                        }
+                        
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            Menu {
+                                Button("Delete", systemImage: "trash", role: .destructive) {
+                                    isPresentingConfirm = true
                                 }
-                                .confirmationDialog("Deleting this move type will delete all related moves. Are you sure?", isPresented: $isPresentingConfirm) {
-                                    Button("Delete this move type permanently", role: .destructive) {
-                                        deleteMoveType()
-                                    }
+                            } label: {
+                                Image(systemName: "ellipsis.circle")
+                            }
+                            .confirmationDialog("Deleting this move type will delete all related moves. Are you sure?", isPresented: $isPresentingConfirm) {
+                                Button("Delete this move type permanently", role: .destructive) {
+                                    deleteMoveType()
                                 }
                             }
                         }
+                    }
                 }
-                
                 .navigationTitle("Edit \(moveType.name)")
                 .navigationBarTitleDisplayMode(.inline)
-                
             }
             .ignoresSafeArea(.keyboard)
-
-           
-         
-            .background(
-                backgroundGradient
-            )
-       
-            
-         
+            .background(backgroundGradient)
+            .alert(errorTitle, isPresented: $showingError) {
+            } message: {
+                Text(errorMessage)
+            }
         }
-        
-
-
-       
-        
-        
-        
     }
     
+    init(moveType: MoveType) {
+        self.moveType = moveType
+        _typeName = .init(initialValue: moveType.name)
+    }
     
+    private func validateTypeName() -> Bool {
+        let trimmedName = typeName.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard !trimmedName.isEmpty else {
+            inputError(title: "Name Error", message: "Move type name cannot be empty.")
+            return false
+        }
+        
+        return true
+    }
     
+    private func inputError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
     
     private func deleteMoveType() {
-        
-        
         moveType.moves.forEach { modelContext.delete($0) }
         modelContext.delete(moveType)
         try? modelContext.save()
         dismiss()
-      
-        
     }
-    
-    
-    init(moveType: MoveType) {
-        
-        self.moveType = moveType
-        _typeName = .init(initialValue: moveType.name)
-        
-    }
-        
-        
-        
-        
-        
-        
-    }
-
+}
 
 #Preview {
     let sample = MoveType.example
